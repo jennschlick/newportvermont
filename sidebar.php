f06f904810973164100cb94e5e0c2973c2e076dd
Jenn Schlick <jlschlick@gmail.com>
2019-06-15 06:30:36 -0400

    Change page sidebars to use menus instead of child pages.

--- sidebar.php
+++ sidebar.php
@@ -1,37 +1,22 @@
-<?php
- if($post->post_parent) {
-  $parent_title = get_the_title($post->post_parent);
-  $parent_url = get_permalink($post->post_parent);
-  echo '<p><a href="';
-  echo $parent_url;
-  echo '">';
-  echo $parent_title;
-  echo '</a>:</p>';
- }
- else {
-  echo '<p><a href="';
-  echo get_permalink($post->ID);
-  echo '">';
-  echo get_the_title($post->ID);
-  echo '</a>:</p>';
- }
-?>
-<?php
-if ( $post->post_parent ) {
-  $children = wp_list_pages( array(
-    'title_li' => '',
-    'child_of' => $post->post_parent,
-    'echo'     => 0
-  ) );
-} else {
-  $children = wp_list_pages( array(
-    'title_li' => '',
-    'child_of' => $post->ID,
-    'echo'     => 0
-  ) );
-}
-if ( $children ) : ?>
-  <ul>
-    <?php echo $children; ?>
-  </ul>
+<?php if ( get_field('sidebar_menu') ) : ?>
+  <?php
+   if($post->post_parent) {
+    $parent_title = get_the_title($post->post_parent);
+    $parent_url = get_permalink($post->post_parent);
+    echo '<p><a href="';
+    echo $parent_url;
+    echo '">';
+    echo $parent_title;
+    echo '</a>:</p>';
+   }
+   else {
+    echo '<p><a href="';
+    echo get_permalink($post->ID);
+    echo '">';
+    echo get_the_title($post->ID);
+    echo '</a>:</p>';
+   }
+  ?>
+  <?php $shortcode = get_post_meta($post->ID,'sidebar_menu', true);
+  echo do_shortcode($shortcode); ?>
 <?php endif; ?>
